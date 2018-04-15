2915bc9fc01862443b24dfc0e373f8a3c69c51f0
Aodos <arthur.tng.marques@gmail.com>
2018-04-15 19:18:08 -0300

    Editei NetworkMain e CHECKSUM

--- NetworkProjectz/src/NetworkMain.java
+++ NetworkProjectz/src/NetworkMain.java
@@ -1,5 +1,4 @@
 
-
 import javax.swing.JOptionPane;
 
 public class NetworkMain {
@@ -7,48 +6,61 @@ public class NetworkMain {
 	public static void main(String[] args) {
 		Paridade p = new Paridade();
 		Hamming h = new Hamming();
-		Crc c= new Crc();
+		Crc c = new Crc();
 		Checksum v = new Checksum();
-		String opcoes = "Selecione técnica de detecção:\n"
-					+ "1 - Paridade.\n"
-					+ "2 - CRC\n"
-					+ "3 - CHECKSUM\n"
-					+ "4 - Hamming\n";
+		String opcoes = "Selecione técnica de detecção:\n" + "1 - Paridade.\n" + "2 - CRC\n" + "3 - CHECKSUM\n"
+				+ "4 - CHECKSUM-RECEPTOR\n" + "5 - Hamming\n";
 		String opcaoSelecionada = "";
-		
+
 		do {
 			opcaoSelecionada = JOptionPane.showInputDialog(null, opcoes);
-			
+
 			switch (opcaoSelecionada) {
 			case "1":
 				System.out.println("Paridade");
 				p.iniciaTecnica(JOptionPane.showInputDialog("Informe cadeia de bits"));
-				
+
 				break;
 			case "2":
-				
-                     System.out.println("CRC");
-                     c.Emissor();
-				break;			
+
+				System.out.println("CRC");
+				c.Emissor();
+				break;
 			case "3":
 				System.out.println("CHECKSUM");
 				v.emissor();
+
+				switch (JOptionPane.showInputDialog(
+						"Deseja enviar bloco: " + v.msgParaEnvio + " para receptor??\n" + "1-SIM\n" + "2-NAO\n ")) {
+				case "1":
+					v.receptor(v.msgParaEnvio);
+					break;
+				case "2":
+					break;
+				}
+				
+				//necess[ario p limpar variaveis e nao somas msgs
+				v.msgFinalEmissor = "";
+				v.msgFinalReceptor = "";
+				v.msgEmissor = "";
+				v.msgParaEnvio = "";
+
 				break;
 			case "4":
 				String bloco;
-				String mensagemJP = "Digite o bloco a ser recebido: " ;
-				 bloco= JOptionPane.showInputDialog(mensagemJP);
-				 v.receptor(bloco);
+				String mensagemJP = "Digite o bloco a ser recebido: ";
+				bloco = JOptionPane.showInputDialog(mensagemJP);
+				v.receptor(bloco);
 				break;
-				
+
 			case "5":
 				System.out.println("Hamming");
 				h.hamming();
 				break;
 			}
-			
-		}while(opcaoSelecionada != null);
-		
+
+		} while (opcaoSelecionada != null);
+
 	}
 
 }
